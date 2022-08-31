fbf77d7e1a2fc23f7957def41a07a3a4548322f1
FredLikesNoodles <82772444+FredLikesNoodles@users.noreply.github.com>
2022-08-31 16:40:07 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX = 0
+leftWristY = 0;
+rightWristY = 0;
+rightWristX = 0;
 music = "";
 function preload() {
   music = loadSound("music.mp3");
@@ -7,6 +11,8 @@ function setup() {
   background("white");
   video = createCapture(VIDEO);
   video.hide();
+  posenet = ml5.poseNet(video, modelLoaded)
+  posenet.on("pose", gotPoses)
 }
 function draw() {
   image(video, 0, 0, 600, 500);
@@ -14,4 +20,21 @@ function draw() {
 
 function play() {
   music.play();
+  music.setVolume(0.5)
+  music.rate(1)
+}
+
+function modelLoaded(){
+    console.log("PoseNet has been intialized")
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results)
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristY = results[0].pose.rightWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log(leftWristX, leftWristX, rightWristX, rightWristY)
+
+    }
 }
