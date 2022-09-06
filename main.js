ded4ab68568d2851116c001ccbc0d62766045595
FredLikesNoodles <82772444+FredLikesNoodles@users.noreply.github.com>
2022-09-06 16:48:35 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ leftWristX = 0
 leftWristY = 0;
 rightWristY = 0;
 rightWristX = 0;
+scoreLeftWrist = 0;
 music = "";
 function preload() {
   music = loadSound("music.mp3");
@@ -16,6 +17,18 @@ function setup() {
 }
 function draw() {
   image(video, 0, 0, 600, 500);
+  fill("red")
+  stroke("red")
+  if(scoreLeftWrist > 0.2){
+    circle(leftWristX, leftWristY, 20)
+    leftWristtoNumber = Number(leftWristY)
+    floored_number = Math.floor(leftWristtoNumber)
+    volume = floored_number/1000
+    v1 = volume*2;
+    music.setVolume(v1)
+    document.getElementById("volume").innerHTML = v1
+  }
+
 }
 
 function play() {
@@ -35,6 +48,7 @@ function gotPoses(results){
         rightWristY = results[0].pose.rightWrist.y;
         rightWristX = results[0].pose.rightWrist.x;
         console.log(leftWristX, leftWristX, rightWristX, rightWristY)
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
 
     }
 }
