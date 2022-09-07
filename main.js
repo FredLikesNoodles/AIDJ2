b618242d43e4cffdd95b5bf800f8271059b7586f
FredLikesNoodles <82772444+FredLikesNoodles@users.noreply.github.com>
2022-09-07 16:32:03 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristY = 0;
 rightWristY = 0;
 rightWristX = 0;
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 music = "";
 function preload() {
   music = loadSound("music.mp3");
@@ -28,6 +29,31 @@ function draw() {
     music.setVolume(v1)
     document.getElementById("volume").innerHTML = v1
   }
+  if(scoreRightWrist > 0.2){
+    circle(rightWristX, rightWristY, 20)
+    if(rightWristY > 0 && rightWristY <= 100){
+      document.getElementById("speed").innerHTML = " 0.5x"
+      music.rate(0.5)
+    }
+    else if(rightWristY > 100 && rightWristY <= 200){
+      document.getElementById("speed").innerHTML = " 1x"
+      music.rate(1)
+    }
+    else if(rightWristY > 200 && rightWristY <= 300){
+      document.getElementById("speed").innerHTML = " 1.5x"
+      music.rate(1.5)
+    }
+    else if(rightWristY > 300 && rightWristY <= 400){
+      document.getElementById("speed").innerHTML = " 2x"
+      music.rate(2)
+    }
+    else if(rightWristY > 400 && rightWristY <= 500){
+      document.getElementById("speed").innerHTML = " 2.5"
+      music.rate(2.5)
+    }
+    
+
+  }
 
 }
 
@@ -49,6 +75,7 @@ function gotPoses(results){
         rightWristX = results[0].pose.rightWrist.x;
         console.log(leftWristX, leftWristX, rightWristX, rightWristY)
         scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
 
     }
 }
